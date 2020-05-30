fbdd7af8471e2c1989514046ce26ecb60a799ba3
jiheonkim21 <59288366+jiheonkim21@users.noreply.github.com>
2020-05-30 13:07:55 -0500

    Add files via upload

---
+++ survey_tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE survey_2017 (
+id INT PRIMARY KEY,
+gender VARCHAR,
+race VARCHAR,
+country VARCHAR,
+education_level VARCHAR,
+undergrad_major VARCHAR,
+years_coding VARCHAR,
+dev_type VARCHAR,
+salary VARCHAR);
+
+CREATE TABLE survey_2018 (
+id INT PRIMARY KEY,
+gender VARCHAR,
+race VARCHAR,
+country VARCHAR,
+education_level VARCHAR,
+undergrad_major VARCHAR,
+years_coding VARCHAR,
+dev_type VARCHAR,
+salary VARCHAR);
+
+
+CREATE TABLE survey_2019 (
+id INT PRIMARY KEY,
+gender VARCHAR,
+race VARCHAR,
+country VARCHAR,
+education_level VARCHAR,
+undergrad_major VARCHAR,
+years_coding VARCHAR,
+dev_type VARCHAR,
+salary VARCHAR);
